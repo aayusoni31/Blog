947914932b12ab0c2c0cf8d8abd8688d2897f088
Aayushi Verma <137631538+aayusoni31@users.noreply.github.com>
2025-02-12 22:58:14 +0530

    included Imagekit

---
+++ src/components/Image.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IKImage } from "imagekitio-react";
+export const Image = ({ src, className, h, w, alt }) => {
+  return (
+    <IKImage
+      urlEndpoint={import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT}
+      path={src}
+      className={className}
+      loading="lazy"
+      lqip={{ active: true, quality: 20 }}
+      alt={alt}
+      width={w}
+      height={h}
+    />
+  );
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-
+import { Image } from "./Image";
+import { Link } from "react-router-dom";
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between ">
       {/* LOGO  */}
-      <div className="flex items-center gap-4 text-2xl font-bold ">
-        <img src="./logo.png" className="w-8 h-8" alt="" />
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold ">
+        <Image src="/logo.png" alt="Blog-logo" w={32} h={32} />
         <span>Blogsphere </span>
-      </div>
+      </Link>
       {/* Mobile menu  */}
       <div className="md:hidden">
         {/* mobile button  */}
@@ -19,10 +20,38 @@ export const Navbar = () => {
           {open ? "X" : "≡"}
         </div>
         {/* mobile link list  */}
-        <div></div>
+        <div
+          style={{ backgroundColor: "#EFB6C8" }}
+          className={`w-full h-screen flex flex-col items-center justify-center font-medium gap-8 text-lg  transition-all ease-in-out absolute top-16 ${
+            open ? "-right-0" : "-right-[100%]"
+          } `}
+        >
+          <Link to="/">Home </Link>
+          <Link to="/">Trending </Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About </Link>
+          <Link to="/">
+            <button
+              style={{ backgroundColor: "#8174A0" }}
+              className="py-2 px-4 rounded-3xl  text-white"
+            >
+              Login 🖐
+            </button>
+          </Link>
+        </div>
       </div>
       {/* Desktop menu  */}
-      <div className="hidden md:flex">D</div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <Link to="/">Home </Link>
+        <Link to="/">Trending </Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About </Link>
+        <Link to="/">
+          <button className="py-2 px-4 rounded-3xl bg-slate-800 text-white">
+            Login 🖐
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
