7a7340e5be3421bdee55a470fa586b13282005bd
Aayushi Verma <137631538+aayusoni31@users.noreply.github.com>
2025-07-06 02:43:26 +0530

    Add React Quill for rich text editing in Write component and update dependencies

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -1,7 +1,58 @@
-import React from "react";
-
+import { useUser } from "@clerk/clerk-react";
+import "react-quill-new/dist/quill.snow.css";
+import ReactQuill from "react-quill-new";
 const Write = () => {
-  return <div></div>;
+  const { isLoaded, isSignedIn } = useUser();
+  if (!isLoaded) {
+    return <div className="">Loading...</div>;
+  }
+  if (isLoaded && !isSignedIn) {
+    return <div className="">You should Login in</div>;
+  }
+  return (
+    <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
+      <h1 className="text-cl font-light">Create a New Post</h1>
+      <form className="flex flex-col gap-6 flex-1  mb-6">
+        <button className="w-max bg-white p-2 shadow-md rounded-xl text-sm text-gray-500">
+          Add a cover image
+        </button>
+        <input
+          className="text-4xl font-semibold bg-transparent outline-none"
+          type="text"
+          placeholder="My Awesome Story"
+        />
+        <div className="flex items-center gap-4">
+          <label htmlFor="" className="text-sm">
+            Choose a category:
+          </label>
+          <select
+            name="cat"
+            id=""
+            className="p-2 rounded-xl bg-white shadow-md"
+          >
+            <option value="general">General</option>
+            <option value="Web Design">Web Design</option>
+            <option value="Tech">Tech</option>
+            <option value="Fashion">Fashion</option>
+            <option value="Markerting">Marketing</option>
+            <option value="Database">Database</option>
+          </select>
+        </div>
+        <textArea
+          className="p-4 rounded-xl bg-white shadow-md"
+          name="desc"
+          placeholder="A Short Descrioption"
+        />
+        <ReactQuill
+          theme="snow"
+          className="flex-1 rounded-xl bg-white shadow-md"
+        />
+        <button className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Write;
